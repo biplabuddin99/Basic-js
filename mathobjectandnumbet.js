bc0c1e1b89402a0a6e732430de7bd5168b6d6e7d
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-06-08 19:44:14 +0600

    mathobject

---
+++ mathobjectandnumbet.js
@@ -0,0 +1,14 @@
+/* Math Object 
+round
+ceil
+floor
+trunc
+*/
+let x=4.2;
+let y=9;
+let z=-5;
+console.log(Math.round(x),Math.ceil(x),Math.floor(x),Math.trunc(x),Math.sign(x),Math.sqrt(y),Math.pow(x,y),Math.abs(z));
+
+let a=(1,4,5,7,9,11);
+console.log(Math.min(1,4,5,7,9,11),Math.max(1,4,5,7,9,11,87,55));
+console.log(Math.round(Math.random()*10000));
