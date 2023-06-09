de9c3c78f7d147206f801969b4e0c605ba920441
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-06-09 16:30:20 +0600

    update

---
+++ arraymethod.js
@@ -0,0 +1,26 @@
+/* Array Method */
+
+let countries=['Mexico','France','USA','Denmark','Germany','Italy','Spain','Poland'];
+console.log(countries.length);
+
+/* Entry/Insert
+1.Push  --------last entry
+2.Unshift  ------first entry
+*/
+countries.push("Brazil");
+countries.push("Argentina");
+console.log(countries);
+
+countries.unshift('Tunisia');
+countries.unshift('Saudi Arobia');
+console.log(countries);
+
+/*Ejech Entry 
+1.pop  --------last left
+2.shift -------first left
+*/
+countries.pop();
+console.log(countries);
+countries.shift();
+ console.log(countries);
+
