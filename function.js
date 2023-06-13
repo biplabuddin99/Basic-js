4a5f50d1dad76768082b26eff4d6f87c194a60ac
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-06-13 17:46:06 +0600

    update

---
+++ function.js
@@ -0,0 +1,34 @@
+/* Es6 -------function */
+// let x=6;
+// let y=9;
+// console.log(x+y);
+
+// function add(x,y){
+//     return x+y;
+// }
+// console.log(add(8,9));
+// console.log(add(8,8));
+// console.log(add(8,7));
+// console.log(add(8,6));
+// console.log(add(8,5));
+
+
+// function fullName(first,last){
+//     return first+''+last;
+// }
+
+// console.log(fullName('Biplab','Uddin'));
+// console.log(fullName('Biplab99','Uddin'));
+// console.log(fullName('Biplab00','Uddin'));
+// console.log(fullName('Biplab66','Uddin'));
+// console.log(fullName('Biplab55','Uddin'));
+
+// function toCelsius(fahren){
+//     return (5/9)*(fahren-32);
+// }
+// console.log(toCelsius(112));
+
+let toCelsius=(fahren)=>{
+    return (5/9)*(fahren-32);
+}
+console.log(toCelsius(112));
