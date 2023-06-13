146de662f24c57f11e29eaf95625cd447304bb1b
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-06-13 15:23:38 +0600

    conditional

---
+++ conditional.js
@@ -0,0 +1,20 @@
+let x=70;
+if(x<18){
+    console.log("You Not eligible NID Card");
+}else if(x>18 && x<50){
+console.log("YOu Have Nid card");
+}else if(x>50 && x<100){
+    console.log("Plese consult with NID officer");
+}
+
+let color ="yellow";
+switch(color){
+    case "green":
+        console.log("this color is green");
+        break;
+        case "red":
+            console.log("this color is red");
+            break;
+            default:
+                console.log("This color is unknown");
+}
