31ac30ea3dd5b2007955e58801b4898b85235e84
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-06-16 14:47:17 +0600

    update

---
+++ OOP/oop2.js
@@ -0,0 +1,24 @@
+class Car{
+    constructor(brand){
+        this.brand=brand;
+    }
+    getInfo=()=>{
+        return 'This car brand name is ' + this.brand;
+    }
+}
+class CarModel extends Car{
+    constructor(brand,model){
+        super(brand);
+        this.model=model;
+    }
+    printInfo=()=>{
+        console.log(this.getInfo()+' and its model number is '+ this.model);
+    }
+}
+
+const mazda=new CarModel('mazda','RX-8');
+const lancer=new CarModel('Lancer','Evo-9');
+const porche=new CarModel('Porche','911');
+
+mazda.printInfo();
+lancer.printInfo();
