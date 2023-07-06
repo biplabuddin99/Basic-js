73d07ddcfa200f4096f07461f14149263391a37b
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-07-06 21:51:07 +0600

    dom multiple class

--- Dom/main.js
+++ Dom/main.js
@@ -25,10 +25,34 @@ document.querySelector()
 // document.getElementById('t-title').innerText='Task List';
 // document.getElementById('t-title').innerHTML='<span style="color:#a855f7">Task</span>';
 
-let val=document.querySelector('#t-title');
-val=document.querySelectorAll('.card-body');
-//console.log(val);
-document.querySelector('li').style.color='red';
-document.querySelector('li:last-child').style.color='red';
-document.querySelector('li:nth-child(3)').style.color='red';
-document.querySelector('li:nth-child(4)').textContent='Biplab';
+
+
+// let val=document.querySelector('#t-title');
+// val=document.querySelectorAll('.card-body');
+// //console.log(val);
+// document.querySelector('li').style.color='red';
+// document.querySelector('li:last-child').style.color='red';
+// document.querySelector('li:nth-child(3)').style.color='red';
+// document.querySelector('li:nth-child(4)').textContent='Biplab';
+
+
+// var listItem=document.getElementsByClassName('list-group-item');
+// //console.log(listItem);
+// listItem[0].style.color='red';
+// listItem[1].textContent='Hello World';
+
+const listItem=document.querySelector('ul').getElementsByClassName('list-group-item');
+console.log(listItem);
+const list=document.querySelectorAll('.list-group-item');
+list.forEach((item,index)=>{
+
+    console.log(item.className,item,index);
+    // item.textContent=index+' :Biplab Uddin';
+    item.style.color='#fff';
+    item.style.backgroundColor='red';
+    item.style.marginTop='2px';
+    item.style.borderRadius='10px';
+    item.style.fontWeight=600;
+    item.textContent=`${index+1}  :Biplab Uddin`;
+})
+//list[0].style.color='red';
