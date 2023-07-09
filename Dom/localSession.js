578846bba24d3f4709a58c1190bdb238b4efa80a
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-07-09 17:46:42 +0600

    session storage

--- Dom/localSession.js
+++ Dom/localSession.js
@@ -1,12 +1,30 @@
-/*  local storage */
-let name='Biplab Uddin';
-localStorage.setItem('secret',name); //value set  -----> key:value;
+// /*  local storage */
+// let name='Biplab Uddin';
+// localStorage.setItem('secret',name); //value set  -----> key:value;
+
+// let result;
+// result=localStorage.getItem('secret'); //value red
+// console.log(result);
+
+// //Remove item
+// //localStorage.removeItem('secret');
+
+// /* session Storage */
+// sessionStorage.setItem('name',name);
+// console.log(sessionStorage.getItem(name));
+
+
+
+let auth={
+    name: 'Admin',
+    email: 'admin@gmail.com',
+    usercode: 'Nid-123456789',
+    token: 'eyanogaphioahfajkerhzniosahfi',
+};
+
+localStorage.setItem('secret',JSON.stringify(auth));
 
 let result;
-result=localStorage.getItem('secret'); //value red
+result=JSON.parse(localStorage.getItem('secret'));
+console.log(result.name);
 console.log(result);
-
-//Remove item
-//localStorage.removeItem('secret');
-
-/* session Storage */
