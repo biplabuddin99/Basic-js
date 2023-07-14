3cd7de789c50337d4e2f412656089171123ffeeb
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-07-14 17:06:29 +0600

    fetch api json

--- fetch-Api/app.js
+++ fetch-Api/app.js
@@ -4,10 +4,9 @@ const btnAPI=document.getElementById('btn-api');
 
 btnText.addEventListener('click',()=>{
     fetch('data.txt')
-    .then((res) => {
+    .then((res)=>{
         console.log(res);
         return res.text();
-
     }).then((data)=>{
         console.log(data);
         post.innerHTML=data;
@@ -15,3 +14,22 @@ btnText.addEventListener('click',()=>{
         console.log(err);
     });
 });
+
+btnJSON.addEventListener('click',()=>{
+    fetch('posts1.json')
+    .then((res)=>{
+        console.log(res);
+        return res.json();
+    }).then((data)=>{
+        console.log(data);
+        let output='';
+        data.forEach((post)=>{
+            output+=`
+            <li>${post.title}</li>
+            <li>${post.id}</li>
+            <li>${post.userId}</li>
+            `;
+        });
+        post.innerHTML=output;
+    })
+})
